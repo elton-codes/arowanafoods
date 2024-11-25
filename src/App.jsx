f4f37df9505eb8655f46abff7e6cc0b9652b8ed7
elton asamoah <eltonasamoah007@gmail.com>
2024-11-25 16:57:01 +0000

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+  return (
+    <>
+    <div className="bg-purple-400 text-white p-6 text-center">
+      <h1 className="text-2xl font-bold">Welcome To Arowana Foods!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
